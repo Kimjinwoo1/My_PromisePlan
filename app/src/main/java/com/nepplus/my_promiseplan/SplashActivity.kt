1bb5d0398b74b3c7b8258b02b7c7a9dcde7b23a5
smlee <larc21c@gmail.com>
2022-06-03 14:38:55 +0900

    #2 로그인 엑티비티 & 스플레시 엑티비티 연결

--- app/src/main/java/com/nepplus/my_promiseplan/SplashActivity.kt
+++ app/src/main/java/com/nepplus/my_promiseplan/SplashActivity.kt
@@ -1,11 +1,29 @@
 package com.nepplus.my_promiseplan
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.nepplus.my_promiseplan.main.LoginActivity
 
-class SplashActivity : AppCompatActivity() {
+class SplashActivity : BasicActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+        val myHandler = Handler(Looper.getMainLooper())
+
+        myHandler.postDelayed({
+
+            val myIntent : Intent
+            myIntent = Intent(mContext, LoginActivity::class.java)
+            startActivity(myIntent)
+        },3000)
     }
 }
