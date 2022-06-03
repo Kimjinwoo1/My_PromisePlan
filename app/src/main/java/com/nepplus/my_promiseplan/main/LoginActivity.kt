0efb8bd6613278cabaef85e67b54518b339bb679
smlee <larc21c@gmail.com>
2022-06-03 13:18:11 +0900

    1. 로그인 xml, 스플레시 xml 뼈대 생성

---
+++ app/src/main/java/com/nepplus/my_promiseplan/main/LoginActivity.kt
@@ -0,0 +1,27 @@
+package com.nepplus.my_promiseplan.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.nepplus.my_promiseplan.R
+import com.nepplus.my_promiseplan.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_login)
+        setupEvents()
+        setValues()
+    }
+
+    fun setupEvents(){
+
+    }
+
+    fun setValues(){
+
+    }
+}
