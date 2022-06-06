5732ceb7435e116055a14a4a40562aa294ffe399
user <wlsdn090909@naver.com>
2022-06-06 19:29:08 +0900

    #2 BaseActivity 작성

---
+++ app/src/main/java/com/nepplus/my_promiseplan/fragments/BaseFragment.kt
@@ -0,0 +1,27 @@
+package com.nepplus.my_promiseplan.fragments
+
+import android.content.Context
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.nepplus.my_promiseplan.api.APIList
+import com.nepplus.my_promiseplan.api.SeverApi
+import retrofit2.Retrofit
+
+abstract class BaseFragment : Fragment() {
+
+    lateinit var mContext : Context
+
+    lateinit var retrofit : Retrofit
+    lateinit var apiList : APIList
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        mContext = requireContext()
+        retrofit = SeverApi.getRetrofit()
+        apiList = retrofit.create(APIList::class.java)
+    }
+
+    abstract fun setEvents()
+    abstract fun setValues()
+}
