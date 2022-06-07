0925eba03c9bcfe6f7a83b6e7738293548d6ad19
user <wlsdn090909@naver.com>
2022-06-07 21:46:11 +0900

    #2 로그아웃 기능 구현

--- app/src/main/java/com/nepplus/my_promiseplan/fragments/SettingsFragment.kt
+++ app/src/main/java/com/nepplus/my_promiseplan/fragments/SettingsFragment.kt
@@ -1,5 +1,6 @@
 package com.nepplus.my_promiseplan.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,6 +9,10 @@ import androidx.databinding.DataBindingUtil
 import com.nepplus.my_promiseplan.R
 import com.nepplus.my_promiseplan.databinding.FragmentInvitedAppointmentsBinding
 import com.nepplus.my_promiseplan.databinding.FragmentSettingBinding
+import com.nepplus.my_promiseplan.dialogs.CustomAlertDialog
+import com.nepplus.my_promiseplan.main.LoginActivity
+import com.nepplus.my_promiseplan.utils.ContextUtil
+import com.nepplus.my_promiseplan.utils.GlobalData
 
 class SettingsFragment : BaseFragment() {
 
@@ -50,7 +55,29 @@ class SettingsFragment : BaseFragment() {
         }
 
         binding.logoutLayout.setOnClickListener {
+            val alert = CustomAlertDialog(mContext,requireActivity())
+            alert.myDialog()
 
+            alert.binding.titleTxt.text = "로그아웃"
+            alert.binding.bodytxt.text = "정말 로그아웃 하시겠습니까?"
+            alert.binding.contentEdt.visibility = View.GONE
+            alert.binding.positiveBtn.setOnClickListener {
+
+                ContextUtil.clear(mContext)
+
+                GlobalData.loginUser = null
+
+                val myIntent = Intent(mContext, LoginActivity::class.java)
+                myIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                startActivity(myIntent)
+
+                alert.dialog.dismiss()
+
+            }
+
+            alert.binding.negativeBtn.setOnClickListener {
+                alert.dialog.dismiss()
+            }
         }
 
     }
