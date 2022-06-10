784fd7bdce83d78827a8378be60e1ecf0a8c7c1c
smlee <larc21c@gmail.com>
2022-06-10 16:48:59 +0900

    #2 약속 추가하는 엑티비티 xml 작성

--- app/src/main/java/com/nepplus/my_promiseplan/ui/EditAppointmentActivity.kt
+++ app/src/main/java/com/nepplus/my_promiseplan/ui/EditAppointmentActivity.kt
@@ -2,6 +2,7 @@ package com.nepplus.my_promiseplan.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.nepplus.my_promiseplan.BasicActivity
 import com.nepplus.my_promiseplan.R
@@ -19,10 +20,30 @@ class EditAppointmentActivity : BasicActivity() {
     }
 
     override fun setupEvents() {
+        binding.addBtn.setOnClickListener {
+            val inputTitle = binding.titleEdt.toString()
+//            약속제목 정하기
+            if (inputTitle.isBlank()){
+                Toast.makeText(mContext, "약속을 정해주세요.", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+//            약속명 정하기
+            val inputPlaceName = binding.placeNameTxt.text.toString()
+            if (inputPlaceName.isBlank()){
+                Toast.makeText(mContext, "약속 장소명을 정해주세요", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+//            날짜 선택
+
+//            시간 선택
+
+        }
 
     }
 
     override fun setValues() {
+        titleTxt.text = "새 약속 만들기"
 
     }
 }
